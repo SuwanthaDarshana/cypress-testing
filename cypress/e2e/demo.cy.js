1ec8b7e3c9dd1e7cdc02529f36143c1d5969c044
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2024-12-05 23:52:46 +0530

    Reload and Logs

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -14,10 +14,18 @@ describe('Cypress E2E Testing Demo', () => {
 
   it('Assert Element', () => {
     cy.visit('https://example.com')
-    cy.get('h1').should('be.visible')
-    cy.get('p').should('be.visible')
+    cy.get('h1').should('be.visible')    //check H1 element
+    cy.wait(2000)                        //wait 2000 milisecond      
+    cy.get('p').should('be.visible')    //check P lement
   })
 
-  
+  it('Reload and Logs', () => {
+    cy.visit('https://example.com')
+    cy.log('Before Reload')
+    cy.reload()
+    cy.log('After Reload')
+  })
+
+
 
 })
