e1996152e6935c4f603b057dce274269cb24e860
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2024-12-05 21:53:46 +0530

    Assert Element check

--- cypress/e2e/demo.cy.js
+++ cypress/e2e/demo.cy.js
@@ -1,5 +1,21 @@
+
+/// <reference types="Cypress" />
+
 describe('Cypress E2E Testing Demo', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+  it('Assert tUrl', () => {
+    cy.visit('https://example.com')
+    cy.url().should('contain', 'example.com/')
   })
+
+  it('Assert Title', () => {
+    cy.visit('https://example.com')
+    cy.title().should('contain', 'Example Domain')
+  })
+
+  it('Assert Element', () => {
+    cy.visit('https://example.com')
+    cy.get('h1').should('be.visible')
+    cy.get('p').should('be.visible')
+  })
+
 })
