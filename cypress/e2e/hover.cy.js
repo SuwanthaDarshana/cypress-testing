7930857d3e4ab49c5f06880961575237e020c674
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2024-12-25 11:36:16 +0530

    add viewport

--- cypress/e2e/hover.cy.js
+++ cypress/e2e/hover.cy.js
@@ -1,5 +1,8 @@
 describe('cypress hover', () => {
   it('Lets test hover workaround', () => {
+
+    // cy.viewport(800, 600)
+    cy.viewport('macbook-16')
     cy.visit('https://practice-automation.com/hover/')
     cy.get('#mouse_over').trigger('mouseover') //trigger mouse over
     cy.get('#mouse_over').should('contain','You did it!')
